98d9307786029c93d00a009d0b32f4ccec83de1e
Michelangelo Ascalon <mikeascalon@yahoo.com>
2023-10-27 17:20:44 -0700

    summulti pass

--- js/app.js
+++ js/app.js
@@ -10,8 +10,9 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
 
-  let total = (a + b);
-  return [total, `The sum of ${a} and ${b} is ${total}.`]
+  let totalSumAb = (a + b);
+  let sumAbMsg = (`The sum of ${a} and ${b} is ${totalSumAb}.`);
+  return [totalSumAb, sumAbMsg];
 
 }
 
@@ -31,13 +32,14 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
 
-  let total = ( a * b);
-  return [total,`The product of ${a} and ${b} is ${total}.` ]
+  let totalProductAb = (a * b);
+  let totalProductAbMsg = (`The product of ${a} and ${b} is ${totalProductAb}.`);
+  return [totalProductAb, totalProductAbMsg];
 
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -54,11 +56,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let sumAb = sum(a, b)[0];
+  let totalSum = sum(sumAb, c)[0];
 
+  let productAb  = multiply(a, b)[0];
+  let totalProduct = multiply(productAb, c)[0];
+
+  return [totalSum, totalProduct, `${a} and ${b} and ${c} sum to ${totalSum}.`, `The product of ${a} and ${b} and ${c} is ${totalProduct}.`];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
